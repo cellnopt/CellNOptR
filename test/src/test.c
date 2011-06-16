ec75298ff84c269cc1a7f08ec34dc9977efa9b28
David Henriques <davidh@iim.csic.es>
2011-06-16 17:48:55 +0000

    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/odeBooleanR@29 be9b9435-a577-4725-b17d-0f23881b854c

---
+++ test/src/test.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char string[] = "Hello, world!";
+int boolVar[]={1,2,4};
+int nDigits[]={2,3,4};
+
+int main(void)
+{
+
+	printf("char %d",boolVar[1]);
+
+	return EXIT_SUCCESS;
+}
+
+int convertDec2Bool()
+{
+	puts("I get here");
+}
