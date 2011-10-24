5773b63e8a68048c6e7212cbe6a42a5b50a785e4
Camille Terfve <terfve@ebi.ac.uk>
2011-10-24 13:49:41 +0000

    Add current CellNOptR 4 Biocond directory -- version 0.99.6 is the current one, submitted to Bioc Oct22
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/CNOR4Biocond/CellNOptR@177 be9b9435-a577-4725-b17d-0f23881b854c

--- R/CNORwrap.R
+++ R/CNORwrap.R
@@ -16,9 +16,12 @@
 #15.Optimise t2 (not implemented in this version)
 #16.Write the scaffold and PKN
 #17.Write the report
+
 CNORwrap<-function(paramsList,Data,Model,Name,NamesData,Time=1){
+
 #if the paramsList is set to NA, it means that only default parameters have been provided
 #so we are going to build the parameters list with those
+
 	if(is.na(paramsList[1])){
 		paramsList<-list()
 		paramsList$Data<-Data
@@ -35,29 +38,50 @@ CNORwrap<-function(paramsList,Data,Model,Name,NamesData,Time=1){
 		paramsList$RelTol<-0.1
 		paramsList$verbose<-FALSE
 		}
+		
 #1.Plot the CNOlist
 	plotCNOlist(paramsList$Data)
-	plotCNOlistPDF(CNOlist=paramsList$Data,fileName=paste(Name,"DataPlot.pdf",sep=""))
+	plotCNOlistPDF(
+		CNOlist=paramsList$Data,
+		fileName=paste(Name,"DataPlot.pdf",sep="")
+		)
+		
 #2.Checks data to model compatibility
 	checkSignals(CNOlist=paramsList$Data,Model=paramsList$Model)
+	
 #3.Find the indices, in the model, of the species that are inh/stim/sign
-	Indices<-indexFinder(CNOlist=paramsList$Data,Model=paramsList$Model,verbose=paramsList$verbose)
+	Indices<-indexFinder(
+		CNOlist=paramsList$Data,
+		Model=paramsList$Model,
+		verbose=paramsList$verbose)
+	
 #4. Find the indices of the non-osb/non-contr	
-	NCNOindices<-findNONC(Model=paramsList$Model,indexes=Indices,verbose=paramsList$verbose)
+	NCNOindices<-findNONC(
+		Model=paramsList$Model,
+		indexes=Indices,
+		verbose=paramsList$verbose)
+		
 #5.Cut the nonc off the model
 	NCNOcut<-cutNONC(Model=paramsList$Model, NONCindexes=NCNOindices)
+	
 #6.Recompute the indices
 	IndicesNCNOcut<-indexFinder(CNOlist=paramsList$Data,Model=NCNOcut)
+	
 #7.Compress the model
 	NCNOcutComp<-compressModel(Model=NCNOcut,indexes=IndicesNCNOcut)
+	
 #8.Recompute the indices
 	IndicesNCNOcutComp<-indexFinder(CNOlist=paramsList$Data,Model=NCNOcutComp)
+	
 #9.Expand the gates	
 	NCNOcutCompExp<-expandGates(Model=NCNOcutComp)
+	
 #10.Compute the residual error
-	resE<-ResidualError(CNOlist=paramsList$Data)
+	resE<-residualError(CNOlist=paramsList$Data)
+	
 #11.Prepare for simulation
 	fields4Sim<-prep4Sim(Model=NCNOcutCompExp)
+	
 #12.Optimisation t1	
 	initBstring<-rep(1,length(NCNOcutCompExp$reacID))
 	T1opt<-gaBinaryT1(CNOlist=paramsList$Data,
@@ -76,27 +100,106 @@ CNORwrap<-function(paramsList,Data,Model,Name,NamesData,Time=1){
 		elitism=paramsList$elitism,
 		RelTol=paramsList$RelTol,
 		verbose=paramsList$verbose)
+		
 #13.Plot simulated and experimental results
-	cutAndPlotResultsT1(Model=NCNOcutCompExp,bString=T1opt$bString,SimList=fields4Sim,CNOlist=paramsList$Data,indexList=IndicesNCNOcutComp,plotPDF=TRUE)
+	cutAndPlotResultsT1(
+		Model=NCNOcutCompExp,
+		bString=T1opt$bString,
+		SimList=fields4Sim,
+		CNOlist=paramsList$Data,
+		indexList=IndicesNCNOcutComp,
+		plotPDF=TRUE)
+		
 #14.Plot the evolution of fit
 	pdf(paste(Name,"evolFitT1.pdf",sep=""))
 	plotFit(OptRes=T1opt)
 	dev.off()
 	plotFit(OptRes=T1opt)
+	
 #15.Optimise t2	
 #Not implemented in this version
 	T2opt<-NA
+	
 #16.Write the scaffold and PKN
+#and
+#17.Write the report
 	if(Time==2){
-		writeScaffold(ModelComprExpanded=NCNOcutCompExp,optimResT1=T1opt,optimResT2=T2opt,ModelOriginal=paramsList$Model,CNOlist=paramsList$Data)
-		writeNetwork(ModelOriginal=paramsList$Model,ModelComprExpanded=NCNOcutCompExp,optimResT1=T1opt,optimResT2=T2opt,CNOlist=paramsList$Data)
-#17.Write the report	
-		namesfiles<-list(dataPlot=paste(Name,"DataPlot.pdf",sep=""),evolFit1=paste(Name,"evolFitT1.pdf",sep=""),evolFit2=paste(Name,"evolFitT2.pdf",sep=""),SimResults2="NCNOcutCompExpSimResultsT1T2.pdf",SimResults1="NCNOcutCompExpSimResultsT1.pdf",Scaffold="NCNOcutCompExpScaffold.sif",ScaffoldDot="ModelModelComprExpandedScaffold.dot",tscaffold="NCNOcutCompExpTimesScaffold.EA",wscaffold="NCNOcutCompExpweightsScaffold.EA",PKN="paramsList$ModelPKN.sif",PKNdot="paramsList$ModelPKN.dot",wPKN="paramsList$ModelTimesPKN.EA",nPKN="paramsList$ModelnodesPKN.NA")
-		writeReport(ModelOriginal=paramsList$Model,ModelOpt=NCNOcutCompExp,optimResT1=T1opt,optimResT2=T2opt,CNOlist=paramsList$Data,directory=Name,namesFiles=namesfiles,namesData=NamesData,resE=resE)	
+#not implemented in this version
+
+		writeScaffold(
+			ModelComprExpanded=NCNOcutCompExp,
+			optimResT1=T1opt,
+			optimResT2=T2opt,
+			ModelOriginal=paramsList$Model,
+			CNOlist=paramsList$Data)
+		writeNetwork(
+			ModelOriginal=paramsList$Model,
+			ModelComprExpanded=NCNOcutCompExp,
+			optimResT1=T1opt,
+			optimResT2=T2opt,
+			CNOlist=paramsList$Data)
+		namesfiles<-list(
+			dataPlot=paste(Name,"DataPlot.pdf",sep=""),
+			evolFit1=paste(Name,"evolFitT1.pdf",sep=""),
+			evolFit2=paste(Name,"evolFitT2.pdf",sep=""),
+			SimResults2="NCNOcutCompExpSimResultsT1T2.pdf",
+			SimResults1="NCNOcutCompExpSimResultsT1.pdf",
+			Scaffold="Scaffold.sif",
+			ScaffoldDot="Scaffold.dot",
+			tscaffold="TimesScaffold.EA",
+			wscaffold="weightsScaffold.EA",
+			PKN="PKN.sif",
+			PKNdot="PKN.dot",
+			wPKN="TimesPKN.EA",
+			nPKN="nodesPKN.NA")
+		writeReport(
+			ModelOriginal=paramsList$Model,
+			ModelOpt=NCNOcutCompExp,
+			optimResT1=T1opt,
+			optimResT2=T2opt,
+			CNOlist=paramsList$Data,
+			directory=Name,
+			namesFiles=namesfiles,
+			namesData=NamesData,
+			resE=resE)	
+	
 		}else{
-			writeScaffold(ModelComprExpanded=NCNOcutCompExp,optimResT1=T1opt,optimResT2=NA,ModelOriginal=paramsList$Model,CNOlist=paramsList$Data)
-			writeNetwork(ModelOriginal=paramsList$Model,ModelComprExpanded=NCNOcutCompExp,optimResT1=T1opt,optimResT2=NA,CNOlist=paramsList$Data)
-			namesfiles<-list(dataPlot=paste(Name,"DataPlot.pdf",sep=""),evolFit1=paste(Name,"evolFitT1.pdf",sep=""),evolFit2=NA,SimResults2=NA,SimResults1="NCNOcutCompExpSimResultsT1.pdf",Scaffold="NCNOcutCompExpScaffold.sif",ScaffoldDot="ModelModelComprExpandedScaffold.dot",tscaffold="NCNOcutCompExpTimesScaffold.EA",wscaffold="NCNOcutCompExpweightsScaffold.EA",PKN="paramsList$ModelPKN.sif",PKNdot="paramsList$ModelPKN.dot",wPKN="paramsList$ModelTimesPKN.EA",nPKN="paramsList$ModelnodesPKN.NA")
-			writeReport(ModelOriginal=paramsList$Model,ModelOpt=NCNOcutCompExp,optimResT1=T1opt,optimResT2=NA,CNOlist=paramsList$Data,directory=Name,namesFiles=namesfiles,namesData=NamesData,resE=resE)
+		
+			writeScaffold(
+				ModelComprExpanded=NCNOcutCompExp,
+				optimResT1=T1opt,
+				optimResT2=NA,
+				ModelOriginal=paramsList$Model,
+				CNOlist=paramsList$Data)
+			writeNetwork(
+				ModelOriginal=paramsList$Model,
+				ModelComprExpanded=NCNOcutCompExp,
+				optimResT1=T1opt,
+				optimResT2=NA,
+				CNOlist=paramsList$Data)
+			namesfiles<-list(
+				dataPlot=paste(Name,"DataPlot.pdf",sep=""),
+				evolFit1=paste(Name,"evolFitT1.pdf",sep=""),
+				evolFit2=NA,SimResults2=NA,
+				SimResults1="NCNOcutCompExpSimResultsT1.pdf",
+				Scaffold="Scaffold.sif",
+				ScaffoldDot="Scaffold.dot",
+				tscaffold="TimesScaffold.EA",
+				wscaffold="weightsScaffold.EA",
+				PKN="PKN.sif",
+				PKNdot="PKN.dot",
+				wPKN="TimesPKN.EA",
+				nPKN="nodesPKN.NA")
+			writeReport(
+				ModelOriginal=paramsList$Model,
+				ModelOpt=NCNOcutCompExp,
+				optimResT1=T1opt,
+				optimResT2=NA,
+				CNOlist=paramsList$Data,
+				directory=Name,
+				namesFiles=namesfiles,
+				namesData=NamesData,
+				resE=resE)
+	
 			}
 }
--- R/plotCNOlist.R
+++ R/plotCNOlist.R
@@ -1,59 +1,86 @@
-plotCNOlist <-
-function(CNOlist){
+plotCNOlist<-function(CNOlist){
+
 #check that CNOlist is a CNOlist
-	if(!is.list(CNOlist)) stop("This function expects as input a CNOlist as output by makeCNOlist or normaliseCNOlist")
-	if(all(names(CNOlist) != c("namesCues","namesStimuli","namesInhibitors","namesSignals","timeSignals","valueCues","valueInhibitors","valueStimuli","valueSignals"))){
+
+	if(!is.list(CNOlist)){
+		stop("This function expects as input a CNOlist as output by makeCNOlist or normaliseCNOlist")
+		}
+	if(all(names(CNOlist) != c(
+		"namesCues",
+		"namesStimuli",
+		"namesInhibitors",
+		"namesSignals",
+		"timeSignals",
+		"valueCues",
+		"valueInhibitors",
+		"valueStimuli",
+		"valueSignals"))){
 		stop("This function expects as input a CNOlist as output by makeCNOlist")
 		}	
-	par(mfrow=c(nr=dim(CNOlist$valueSignals[[1]])[1]+1,nc=dim(CNOlist$valueSignals[[1]])[2]+1),cex=0.5,pch=20,mar=c(0.5,0.5,0,0),oma=c(3,2,2,2))
+		
+#set graphic parameters		
+	par(
+		mfrow=c(nr=dim(CNOlist$valueSignals[[1]])[1]+1,nc=dim(CNOlist$valueSignals[[1]])[2]+1),
+		cex=0.5,pch=20,mar=c(0.5,0.5,0,0),oma=c(3,2,2,2))
 	yMax<-max(unlist(lapply(CNOlist$valueSignals,function(x) max(x, na.rm=TRUE))))
 	yMin<-min(unlist(lapply(CNOlist$valueSignals,function(x) min(x, na.rm=TRUE))))
 	xVal<-CNOlist$timeSignals
+	
 	for(c in 1:dim(CNOlist$valueSignals[[1]])[2]){
 		par(fg="blue",mar=c(0.5,0.5,0.7,0))
-		plot(x=xVal, y=rep(-5,length(xVal)), ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-		text(labels=CNOlist$namesSignals[c],x=((xVal[length(xVal)]-xVal[1])/2),y=(yMin+((yMax-yMin)/2)),cex=2)
+		plot(
+			x=xVal, 
+			y=rep(-5,length(xVal)), 
+			ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
+		text(
+			labels=CNOlist$namesSignals[c],
+			x=((xVal[length(xVal)]-xVal[1])/2),
+			y=(yMin+((yMax-yMin)/2)),cex=2)
 		}
-		plot(x=xVal, y=rep(-5,length(xVal)), ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-		text(labels="Cues",x=((xVal[length(xVal)]-xVal[1])/2),y=(yMin+((yMax-yMin)/2)),cex=2)
-	par(fg="black",mar=c(0.5,0.5,0,0))	
+		
+		plot(
+			x=xVal, 
+			y=rep(-5,length(xVal)), 
+			ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
+		text(
+			labels="Cues",
+			x=((xVal[length(xVal)]-xVal[1])/2),
+			y=(yMin+((yMax-yMin)/2)),cex=2)
+			
+	par(fg="black",mar=c(0.5,0.5,0,0))
+	
 	for(r in 1:dim(CNOlist$valueSignals[[1]])[1]){
+	
 		for(c in 1:dim(CNOlist$valueSignals[[1]])[2]){
 			yVal<-lapply(CNOlist$valueSignals,function(x) {x[r,c]})
-			if(r == dim(CNOlist$valueSignals[[1]])[1] && c== 1){
-				plot(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				lines(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
+			
+			plot(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
+			lines(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
+			
+			#add the annotation of the axis: if we're on the last row we need an x-axis
+			if(r == dim(CNOlist$valueSignals[[1]])[1]){
 				axis(side=1,at=xVal)
-				axis(side=2,at=c(-0.5,0,0.5))
 				}	
-			if(r == dim(CNOlist$valueSignals[[1]])[1] && c != 1){
-				plot(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				lines(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				axis(side=1,at=xVal)
-				}
-			if(c == 1 && r == 1){
-				plot(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				lines(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
+			
+			#add the annotation of the axis: if we're on the first column we need a y-axis
+			if(c == 1){
 				axis(side=2,at=c(-0.5,0,0.5))
 				}
-			if(c == 1 && r != 1 && r != dim(CNOlist$valueSignals[[1]])[1]){	
-				plot(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				lines(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				axis(side=2,at=c(-0.5,0,0.5))
-				}
-			if(r == 1 && c!= 1){
-				plot(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				lines(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				}
-			if(c != 1 && r!=1 && r!=dim(CNOlist$valueSignals[[1]])[1] && c != dim(CNOlist$valueSignals[[1]])[1]+1){
-				plot(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				lines(x=xVal,y=yVal,ylim=c(yMin, yMax),xlab=NA,ylab=NA,xaxt="n",yaxt="n")
-				}
+				
 			}
-		image(t(matrix(c(CNOlist$valueStimuli[r,],CNOlist$valueInhibitors[r,]),nrow=1)),col=c("white","black"),xaxt="n",yaxt="n")	
+			
+		image(
+			t(matrix(c(CNOlist$valueStimuli[r,],CNOlist$valueInhibitors[r,]),nrow=1)),
+			col=c("white","black"),xaxt="n",yaxt="n")	
+		
 		if(r == dim(CNOlist$valueSignals[[1]])[1]){
-			axis(side=1,at=seq(from=0, to=1,length.out=length(CNOlist$namesCues)),labels=c(CNOlist$namesStimuli,paste(CNOlist$namesInhibitors,"-i",sep="")),las=3,cex.axis=1)
+			axis(
+				side=1,
+				at=seq(from=0, to=1,length.out=length(CNOlist$namesCues)),
+				labels=c(CNOlist$namesStimuli,paste(CNOlist$namesInhibitors,"-i",sep="")),
+				las=3,cex.axis=1)
 			}
+			
 		}
 		
 	}
