ce4a6970477dfdc218e32abb585887d07d6b3bcb
David Henriques <davidh@iim.csic.es>
2011-11-08 16:19:22 +0000

    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CNOR_ode@263 be9b9435-a577-4725-b17d-0f23881b854c

--- tests/test3/example.R
+++ tests/test3/example.R
@@ -11,144 +11,32 @@
 
 # First read the MIDAS and SIF file to get some data
 
-makeParameterList=function(adjMat,namesSpecies)
-{
-	isState=getStates(adjMat);
-	numStates=as.integer(sqrt(length(adjMat)));
-	parNames=c();
-	parValues=c();
-	count=0;
-	for(j in 1:numStates)
-	{ 
-		if(isState[j])
-		{
-			jCol=adjMat[,j];
-			inputs=which(as.logical(jCol));
-			numInputs=length(inputs);
-			for(i in 1:numInputs)
-			{
-				count=count+1;
-				parNames[count]=paste(namesSpecies[inputs[i]],"_n_",namesSpecies[j],sep="");
-				parValues[count]=3;
-				count=count+1;
-				parNames[count]=paste(namesSpecies[inputs[i]],"_k_",namesSpecies[j],sep="");
-				parValues[count]=0.5;
-			}
-			count=count+1;
-			parNames[count]=paste("tau_",namesSpecies[j],sep="");
-			parValues[count]=1;
-		}
-	}
-	parList=list(parNames=parNames,parValues=parValues);
-	
-	return(parList);
-}
-
-getStates=function(adjacency)
-{
-	nSpecies=dim(adjacency)[1];
-	count=0;
-	res=matrix(0,1,nSpecies);
-	for(j in 1:nSpecies)
-	{
-		for(i in 1:nSpecies)
-		{
-			if(adjacency[i,j])
-			{
-				res[j]=1;
-			}
-		}
-	}
-	return(res);
-}
-
-incidence2Adjacency=function(model)
-{ 
-	incidence=model$interMat;
-	nNodes=dim(incidence)[1];
-	nEdges=dim(incidence)[2];
-	adjacency=matrix(0,nNodes,nNodes);
-	
-	for(j in 1:nEdges)
-	{
-		for(i in 1:nNodes)
-		{
-			if(incidence[i,j]==1)
-			{
-				node1=i;
-				for(k in 1:nNodes)
-				{
-					if(incidence[k,j]==-1)
-					{
-						adjacency[k,node1]=1
-					}
-				}
-			}
-		}
-	}
-	return(adjacency);  
-}
-
-interface <- function(cnolist, sif, indices, odeParameters, time=1,verbose=0)
-{	 
-	interMat <- as.integer(as.vector(t(sif$interMat)))
-	notMat <- as.integer(as.vector(t(sif$notMat)))
-	nRows <- as.integer(dim(sif$interMat)[1])
-	nCols <- as.integer(dim(sif$interMat)[2])
-	verbose=as.integer(verbose);
-	# ode 
-	nPars <- as.integer(length(odeParameters))
-	
-	# cnolist
-	timeSignals <- as.double(cnolist$timeSignals)
-	valueInhibitors <- as.double(t(cnolist$valueInhibitors))
-	valueSignals <- as.double(as.vector(cnolist$valueSignals[[time]]))
-	valueStimuli <- as.double(t(cnolist$valueStimuli))
-	nTimes = as.integer(length(cnolist$timeSignals))
-	
-	# [[1]] allows to access to the first object in the list and retrieve its dimensions
-	nExperiments = as.integer(dim(cnolist$valueSignals[[time]]))
-	
-	#indices
-	nSignals <- as.integer(length(indices$signals))
-	
-	indexSignals <- as.integer(as.vector(indices$signals))
-	nStimuli <- as.integer(length(indices$stimulated))
-	indexStimuli <- as.integer(as.vector(indices$stimulated))
-	nInhibitors <- as.integer(length(indices$inhibited))
-	indexInhibitors <- as.integer(as.vector(indices$inhibited))
-	
-	res = .Call("sim_logic_ode",interMat,notMat,nRows,nCols,nPars,timeSignals,
-		valueInhibitors,valueSignals,valueStimuli,nTimes,nExperiments,nSignals,
-		indexSignals,nStimuli,indexStimuli,nInhibitors,indexInhibitors,
-		odeParameters,verbose);
-
-	return(res);
-	
-}
 
 library(CellNOptR)
-##detach("package:testPackage");
 #install.packages("CNORode_1.0.zip",repos=NULL);
-library("CNORode");
-setwd("C:/Users/davidh/workspace_java/CNOR_ode/tests/test3")
+#setwd("tests/test3");
+library("CNORode")
+
 s = readSif('model.sif')
 m = readMIDAS('initialData.csv')
 cnolist = makeCNOlist(m, subfield=FALSE)
-cnolist$timeSignals=seq(0,10)
 
 indices <- indexFinder(cnolist, s, verbose = TRUE)
 modelNCNOindices <- findNONC(s, indices, verbose = TRUE)
 s <- cutNONC(s, modelNCNOindices);
 
-adjMat=incidence2Adjacency(s)
-odeParameters=makeParameterList(adjMat,s$namesSpecies)
+#results=logic_based_ode_parameters_estimation_SSm(cnolist,s)
+#logic_based_ode_MINLP_SSm(cnolist,s,ndiverse=10,dim_refset=6)
 
-# Finally, call the function
-res = interface(cnolist, s, indices,odeParameters$parValue,verbose=TRUE);
-out<-lapply(res,function(x) x[,indices$signals])
+adjMat=incidence2Adjacency(s);
+ode_parameters=makeParameterList(adjMat,s$namesSpecies);
+simulator<-get_simulation_function(cnolist,s,adjMat,indices,ode_parameters,reltol=1e-3,atol=1e-5);
+
+sim=simulator(cnolist,s,ode_parameters$parValues)
+value_signals<-lapply(sim,function(x) x[,indices$signals]);
+
 plotCNOlist(cnolist)
-windows()
-cnolist$valueSignals=out;
-plotCNOlist(cnolist);
+#windows();
+cnolist$valueSignals=value_signals;
+plotCNOlist(cnolist)
 
