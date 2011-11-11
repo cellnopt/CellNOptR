fdcde7176fa1cbc3e37238d9d986304940fba06b
David Henriques <davidh@iim.csic.es>
2011-11-11 16:51:38 +0000

    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CNOR_ode@287 be9b9435-a577-4725-b17d-0f23881b854c

--- tests/test1/example.R
+++ tests/test1/example.R
@@ -31,8 +31,8 @@ s <- cutNONC(s, modelNCNOindices);
 adjMat=incidence2Adjacency(s);
 ode_parameters=makeParameterList(adjMat,s$namesSpecies,random=TRUE);
 results=logic_based_ode_parameters_estimation_SSm(cnolist,s,ode_parameters=ode_parameters)
-
-#simulator<-get_simulation_function(cnolist,s,adjMat,indices,ode_parameters);
-#sim=simulator(cnolist,s,indices,ode_parameters$parValues)
-#value_signals<-lapply(sim,function(x) x[,indices$signals]);
+ode_parameters$parValues[ode_parameters$index_opt_pars]=results$x_best;
+simulator<-get_simulation_function(cnolist,s,adjMat,indices,ode_parameters);
+sim=simulator(cnolist,s,indices,ode_parameters$parValues)
+value_signals<-lapply(sim,function(x) x[,indices$signals]);
 
