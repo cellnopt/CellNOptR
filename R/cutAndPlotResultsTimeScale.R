2c456102332ee8bad542f69ffe1141bba845ff5c
Aidan Macnamara <aidan.macnamara@ebi.ac.uk>
2011-11-25 08:07:11 +0000

    added CNORdiscreteTime
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CNORdiscreteTime@343 be9b9435-a577-4725-b17d-0f23881b854c

---
+++ R/cutAndPlotResultsTimeScale.R
@@ -0,0 +1,24 @@
+cutAndPlotResultsTimeScale <- function(Model, bString, SimList, CNOlist, indexList, plotPDF=FALSE, boolUpdates=boolUpdates, divTime=NULL) {
+	
+	Modelcut <- Model
+	Modelcut$interMat <- Modelcut$interMat[,as.logical(bString)]
+	Modelcut$notMat <- Modelcut$notMat[,as.logical(bString)]
+	Modelcut$reacID <- Modelcut$reacID[as.logical(bString)]
+	SimListcut <- SimList
+	SimListcut$finalCube <- SimListcut$finalCube[as.logical(bString),]
+	SimListcut$ixNeg <- SimListcut$ixNeg[as.logical(bString),]
+	SimListcut$ignoreCube <- SimListcut$ignoreCube[as.logical(bString),]
+	SimListcut$maxIx <- SimListcut$maxIx[as.logical(bString)]
+	
+	boolUpdates = boolUpdates[1]
+	SimRes <- simulatorTimeScale(CNOlist=CNOlist, Model=Modelcut, SimList=SimListcut, indexList=indexList, boolUpdates=boolUpdates)
+	SimRes = SimRes[,indexList$signals,]
+	getFitData <- getFitTimeScale(SimList=SimListcut, CNOlist=CNOlist, Model=Modelcut, indexList=indexList, boolUpdates=boolUpdates, divTime=divTime)
+	AU <- getFitData$estimate
+	x.coords <- getFitData$x.coords
+	expResults <- CNOlist$valueSignals
+	
+	plotOptimResultsTimeScale(SimResults=SimRes, yInterpol=getFitData$yInter, xCoords=x.coords, expResults=expResults, times=CNOlist$timeSignals, namesCues=CNOlist$namesCues, namesSignals=CNOlist$namesSignals, valueCues=CNOlist$valueCues)
+
+}
+
