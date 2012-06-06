308952dc34a83a173ae010c8c9060d45d454dd0d
Thomas Cokelaer <cokelaer@gmail.com>
2012-06-06 09:30:44 +0000

    faster simulator (3 times)
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@1398 be9b9435-a577-4725-b17d-0f23881b854c

--- R/simulatorT1.R
+++ R/simulatorT1.R
@@ -67,117 +67,125 @@ simulatorT1<-function(CNOlist,Model,SimList,indexList){
     termCheck2<-TRUE
     count<-1
 
+    minNA<-function(x){
+        if(all(is.na(x))){
+            return(NA)
+        }else{
+            return(min(x,na.rm=TRUE))
+        }
+    }
+
+    filltempCube<-function(x){
+        cMatrix<-matrix(data=x,nrow=nReacs,ncol=nCond)
+	    cVector<-apply(cMatrix,1,function(x){return(x)})
+	return(cVector)
+	}
+
+    # The IxNeg and Ignore matrices can be filled once for all before the loop
+    if(nReacs > 1){
+        tempIxNeg<-apply(SimList$ixNeg,2,filltempCube)
+		tempIgnore<-apply(SimList$ignoreCube,2,filltempCube)
+		}
+    else{
+        tempIxNeg<-matrix(
+            SimList$ixNeg,nrow=nCond,
+            ncol=length(SimList$ixNeg),byrow=TRUE)
+        tempIgnore<-matrix(
+            SimList$ignoreCube,nrow=nCond,
+            ncol=length(SimList$ignoreCube),byrow=TRUE)
+    }
+
     #Main loop
-    while(termCheck1 && termCheck2){
+    while(termCheck1 && termCheck2 ){
     
         outputPrev<-newInput
-        # This is now a 2 columns matrix that has a column for each input 
-        # (column in finalCube) and a set of rows for each reac (where a 
-        # set contains as many rows as conditions) all concatenated into one
-        # long column.
-
-        filltempCube<-function(x){
-            cMatrix<-matrix(data=x,nrow=nReacs,ncol=nCond)
-            cVector<-apply(cMatrix,1,function(x){return(x)})
-            return(cVector)
-            }
-
+        # This is now a 2 columns matrix that has a column for each input (column in finalCube)
+        # and a set of rows for each reac (where a set contains as many rows as conditions)
+        # all concatenated into one long column.
         if(nReacs > 1){
-
-            tempStore<-apply(SimList$finalCube,2,
-                function(x){return(outputPrev[,x])})
-            tempIxNeg<-apply(SimList$ixNeg,2,filltempCube)
-            tempIgnore<-apply(SimList$ignoreCube,2,filltempCube)
-
-            }
+            #outputPrev[,SimList$fina
+			tempStore<-apply(SimList$finalCube,2,function(x){return(outputPrev[,x])})
+			}
         else{
-
             tempStore<-outputPrev[,SimList$finalCube]
-            tempIxNeg<-matrix(
-                    SimList$ixNeg,nrow=nCond,
-                    ncol=length(SimList$ixNeg),byrow=TRUE)
-            tempIgnore<-matrix(
-                    SimList$ignoreCube,nrow=nCond,
-                    ncol=length(SimList$ignoreCube),byrow=TRUE)
-
         }
-        # Set to NA the values that are "dummies", so they won't influence the
-        # min.
-        tempStore[tempIgnore]<-NA
 
-        #Flip the values that enter with a negative sign        
-        tempStore[tempIxNeg]<-1-tempStore[tempIxNeg]
-  
-        minNA<-function(x){
-            if(all(is.na(x))){
-                return(NA)
-                }else{
-                    return(min(x,na.rm=TRUE))
+        #Compute all the ands by taking, for each gate, the min value across the inputs of that gate
+		if(nReacs > 1){
+            # new code with inf instead of NA. Speed up the code
+			#Set to Inf the values that are "dummies", so they won't influence the min
+	        tempStore[tempIgnore]<-Inf
+
+			#Flip the values that enter with a negative sign
+			tempStore[tempIxNeg]<-1-tempStore[tempIxNeg]
+
+            outputCube <- apply(tempStore, 1, min)
+			#outputCube<-apply(tempStore,1,minNA)
+
+            #OutputCube is now a vector of length (nCond*nReacs) that contains the input of each reaction in
+            #each condition, concatenated as such allcond4reac1,allcond4reac2,etc...
+            #This is transformed into a matrix with a column for each reac and a row for each cond
+			outputCube<-matrix(outputCube, nrow=nCond, ncol=nReacs)
+
+            #Go through each species, and if it has inputs, then take the max across those input reactions
+            #i.e. compute the ORs
+
+            # create the newInput matrix once for all. Faster than implicit
+            # creation
+            newInput = matrix(ncol=nSp, nrow=nCond, byrow=FALSE)
+			for(s in 1:nSp){
+				if(endIx[s] != 0){
+                    selection <- outputCube[,SimList$maxIx == s]
+                    if (length(selection) == nCond){
+                        newInput[,s] <- selection
                     }
+                    else{
+                        selection[is.na(selection)] <- -Inf
+                        newInput[,s] <- apply(selection, 1, max)
+                    }
+                }
             }
+            newInput[is.infinite(newInput)] <- NA
+        }
+        else{
+            # OLD code with NA. Slower but we hardly enter in this loop
+			#Set to NA the values that are "dummies", so they won't influence the min
+			tempStore[tempIgnore]<-NA
+			#Flip the values that enter with a negative sign
+			tempStore[tempIxNeg]<-1-tempStore[tempIxNeg]
 
-        # Compute all the ands by taking, for each gate, the min value across the
-        # inputs of that gate.
-        if(nReacs > 1){
-            outputCube<-apply(tempStore,1,minNA)
+            outputCube<-ifelse(all(is.na(tempStore)),NA,min(tempStore,na.rm=TRUE))
+            newInput[,SimList$maxIx]<-outputCube
+        }
 
-        # OutputCube is now a vector of length (nCond*nReacs) that contains the
-        # input of each reaction in each condition, concatenated as such
-        # allcond4reac1,allcond4reac2,etc...  This is transformed into a matrix
-        # with a column for each reac and a row for each cond.
-            outputCube<-matrix(outputCube, nrow=nCond,ncol=nReacs)
-            
-        # Go through each species, and if it has inputs, then take the max
-        # across those input reactions i.e. compute the ORs.
-
-            for(s in 1:nSp){
-                if(endIx[s] != 0){
-                    compOR<-function(x){
-                        if(all(is.na(x[which(SimList$maxIx == s)]))){
-                            res<-NA
-                            }else{
-                                res<-max(x[which(SimList$maxIx == s)],na.rm=TRUE)
-                                }
-                        return(res)
-                        }
-                    newInput[,s]<-apply(outputCube,1,compOR)
-                    }
-                }
-            }else{
-                outputCube<-ifelse(all(is.na(tempStore)),NA,min(tempStore,na.rm=TRUE))
-                newInput[,SimList$maxIx]<-outputCube
-                }
-
-        # Reset the inhibitors and stimuli
-        for(stim in 1:length(indexList$stimulated)){
-            stimM<-cbind(
-                CNOlist$valueStimuli[,stim],
-                newInput[,indexList$stimulated[stim]])
-            maxNA<-function(x){
-                return(max(x,na.rm=TRUE))
-                }
-                
+    #Reset the inhibitors and stimuli
+    for(stim in 1:length(indexList$stimulated)){
+			stimM<-cbind(
+			    CNOlist$valueStimuli[,stim],
+				newInput[,indexList$stimulated[stim]])
+			maxNA<-function(x){
+				return(max(x,na.rm=TRUE))
+				}
             stimV<-apply(stimM,1,maxNA)
-            newInput[,indexList$stimulated[stim]]<-stimV
-            }
-            
-        valueInhibitors<-1-CNOlist$valueInhibitors
-        newInput[,indexList$inhibited]<-valueInhibitors*newInput[,indexList$inhibited]
-        
-        # Replace NAs with zeros to avoid having the NA    penalty applying to
-        # unconnected species.
-        newInput[is.na(newInput)]<-0
-        outputPrev[is.na(outputPrev)]<-0
-        
-        # Check the 2 termination conditions        
-        termCheck1<-!all(abs(outputPrev-newInput)<testVal)
-        termCheck2<-(count < (nSp*1.2))
-        count<-count+1
-        }
-        
-    # Set the non-resolved bits to NA
-    newInput[which(abs(outputPrev-newInput) > testVal)]<-NA
-    
-    return(newInput)
-    }
+			newInput[,indexList$stimulated[stim]]<-stimV
+			}
+#
+    valueInhibitors<-1-CNOlist$valueInhibitors
+    newInput[,indexList$inhibited]<-valueInhibitors*newInput[,indexList$inhibited]
+
+    #Replace NAs with zeros to avoid having the NA	penalty applying to unconnected species
+	newInput[is.na(newInput)]<-0
+	outputPrev[is.na(outputPrev)]<-0
+
+    #Check the 2 termination conditions		
+	termCheck1<-!all(abs(outputPrev-newInput)<testVal)
+	termCheck2<-(count < (nSp*1.2))
+	count<-count+1
+	}
+
+#Set the non-resolved bits to NA
+	newInput[which(abs(outputPrev-newInput) > testVal)]<-NA
+
+	return(newInput)
+	}
 
