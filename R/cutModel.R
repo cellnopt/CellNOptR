abee1ebb6571c3a7bd92c4e34c925daa42987757
Thomas Cokelaer <cokelaer@gmail.com>
2012-06-20 17:24:52 +0000

    add new function (for modularity)
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@1530 be9b9435-a577-4725-b17d-0f23881b854c

---
+++ R/cutModel.R
@@ -0,0 +1,30 @@
+#
+#  This file is part of the CNO software
+#
+#  Copyright (c) 2011-2012 - EBI
+#
+#  File author(s): CNO developers (cno-dev@ebi.ac.uk)
+#
+#  Distributed under the GPLv2 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-2.0.html
+#
+#  CNO website: http://www.ebi.ac.uk/saezrodriguez/software.html
+#
+##############################################################################
+# $Id$
+
+cutModel <- function(model, bString){
+
+    bs = as.logical(bString)
+    newmodel<-list()
+    newmodel$interMat<-model$interMat[, bs]
+    newmodel$notMat<-model$notMat[, bs]
+    newmodel$reacID<-model$reacID[bs]
+    # namespecies is not needed
+    # could also add the times used in gaBinaryT2
+
+    return(newmodel)
+}
+
+
