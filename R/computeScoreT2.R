665a2282e55ff2b7df7f46359af0c291f5aaca65
Thomas Cokelaer <cokelaer@gmail.com>
2012-06-25 16:00:45 +0000

    Finalise computeScore function.
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@1574 be9b9435-a577-4725-b17d-0f23881b854c

--- R/computeScoreT2.R
+++ R/computeScoreT2.R
@@ -14,39 +14,43 @@
 ##############################################################################
 # $Id$
 
-#Function that computes the score of a specific bitstring
-# todo: this is similar to wha is done in gaBinaryT1. need to do the same for T2
-# todo: timepoints and nafac are hardcoded.
-computeScoreT2<-function(CNOlist,Model,SimList,indexList,SimResT1, bitString,
-bStringTimes,sizeFac=0.0001, NAFac=1){
+# Function that computes the score of a specific bitstring
+# Although it is very similar to computeScoreT1, there are enough differences to
+# have a different function.
+computeScoreT2<-function(CNOlist, Model, SimList, indexList, SimResT1, bStringT1,
+    bStringT2, sizeFac=0.0001, NAFac=1){
+
+    bitString <- bStringT1
+    bitString[which(bStringT1 == 0)]<- bStringT2
+    bStringTimes <- bStringT1
+    bStringTimes[which(bStringT1 == 0)]<-bStringT2*2
+
+    ModelCut = cutModel(Model, bitString)
+    ModelCut$times <- bStringTimes[which(bStringTimes != 0)]
+    SimListCut <- cutSimList(SimList, bitString)
 
 
+    # We may want to to use the T0 information.
+    SimResults <- simulatorT2(
+        SimResultsT1=SimResT1,
+        CNOlist=CNOlist, 
+        Model=ModelCut,
+        SimList=SimListCut,
+        indexList=indexList)
 
-  ModelCut = cutModel(Model, bitString)
-  ModelCut$times<-bStringTimes[which(bStringTimes != 0)]
-  SimListCut<-cutSimList(SimList, bitString)
+    #Compute the score
+    Score <- getFit(
+        SimResults=SimResults,
+        CNOlist=CNOlist,
+        Model=ModelCut,
+        indexList=indexList,
+        timePoint="t2",
+        sizeFac=sizeFac,
+        NAFac=NAFac,
+        nInTot=length(which(Model$interMat == -1)))
 
+  nDataP <- sum(!is.na(CNOlist$valueSignals[[2]]))
+  Score <- Score/nDataP
 
-  # We may want to to use the T0 information.
-  SimResults<-simulatorT2(
-    SimResultsT1=SimResT1,
-    CNOlist=CNOlist,
-    Model=ModelCut,
-    SimList=SimListCut,
-    indexList=indexList)
-
-  #Compute the score
-  Score<-getFit(
-    SimResults=SimResults,
-    CNOlist=CNOlist,
-    Model=ModelCut,
-    indexList=indexList,
-    timePoint="t2",
-    sizeFac=sizeFac,
-    NAFac=NAFac,
-    nInTot=length(which(Model$interMat == -1)))
-  nDataP<-sum(!is.na(CNOlist$valueSignals[[2]]))
-  Score<-Score/nDataP
-  
   return(Score)
 }
