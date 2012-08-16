d5ea86dbcd3053ccaa62e4455b1645506e96f906
Thomas Cokelaer <cokelaer@gmail.com>
2012-08-16 08:33:57 +0000

    add computeScoreT2 in deprecated lsit.
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@2116 be9b9435-a577-4725-b17d-0f23881b854c

--- R/deprecated.R
+++ R/deprecated.R
@@ -7,3 +7,20 @@ prep4Sim <- function(model, params){
     warning("prep4Sim is a deprecated function. Use prep4sim instead. ")
     return(prep4sim(model))
 }
+
+simulateT1 <- function(CNOlist, model, bStringT1,simList, indexList){
+    warning("simulateT1 is a deprecated function. Use simulate instead. ")
+    return(simulate(CNOlist, model, bStrings=list(bStringT1)))
+}
+
+computeScoreT2 <- function(CNOlist, model, simList=NULL, indexList=NULL, 
+    simResT1, bStringT1, bStringT2, sizeFac=0.0001, NAFac=1){
+
+    warning("computeScoreT2 is a deprecated function. Use computeScoreTN instead.")
+    return(computeScoreTN(CNOlist, model, simList=simList, indexList=indexList, 
+        simResT1, bStringT1,  bStringT2, timeIndex=3, sizeFac=sizeFac,
+        NAFac=NAFac))
+ 
+}
+
+
