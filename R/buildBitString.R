a18eb80aca63398b328996c58251fd8cfbad5ba4
Thomas Cokelaer <cokelaer@gmail.com>
2012-08-17 12:47:34 +0000

    finalised doc of buildBitString
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@2132 be9b9435-a577-4725-b17d-0f23881b854c

--- R/buildBitString.R
+++ R/buildBitString.R
@@ -7,19 +7,31 @@ buildBitString <- function(bStrings){
     #[1] 1 1 0 1 2 3
 
     # build bitstrings from different steady states
+    if (is.list(bStrings)==FALSE){
+        if (is.vector(bStrings, mode="numeric")==TRUE){
+            bStrings = list(bStrings)
+        } else{ 
+            stop("CellNOpt Error: argument bString must be a list of bitstring or a single bitstring. Each bitstring must be a vector. See manual for details.")
+
+        }
+    }
     N = length(bStrings)
-    
+
     if (N < 1 ){
-        stop("bStrings must be a list of length 1")
+        stop("CellNOpt Error: bStrings argument must be a list of length greater or equal to 1")
     }
 
-    bs = bStrings[[1]]
-    bsTimes = bStrings[[1]]
-
-    for (i in 2:N){
-        bs[which(bs==0)] <- bStrings[[i]]
-        bsTimes[which(bsTimes==0)]<-bStrings[[i]]*(i)
-
+    if (N>=1){
+        bs = bStrings[[1]]
+        bsTimes = bStrings[[1]]
     }
+
+    if (N>1){
+        for (i in 2:N){
+            bs[which(bs==0)] <- bStrings[[i]]
+            bsTimes[which(bsTimes==0)]<-bStrings[[i]]*(i)
+        }
+    }
+
     return(list(bs=bs, bsTimes=bsTimes))
 }
