25dcbf1d7159b8c2e4408cae124963a4dc5e63e0
Thomas Cokelaer <cokelaer@gmail.com>
2012-08-17 16:40:31 +0000

    add gaBinaryT2 to deprecated.
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@2152 be9b9435-a577-4725-b17d-0f23881b854c

--- R/deprecated.R
+++ R/deprecated.R
@@ -20,7 +20,18 @@ computeScoreT2 <- function(CNOlist, model, simList=NULL, indexList=NULL,
     return(computeScoreTN(CNOlist, model, simList=simList, indexList=indexList, 
         simResT1, bStringT1,  bStringT2, timeIndex=3, sizeFac=sizeFac,
         NAFac=NAFac))
- 
 }
 
+gaBinaryT2 <- function(CNOlist, model, simList, indexList, bStringT1,
+    sizeFac=0.0001, NAFac=1, popSize=50, pMutation=0.5, maxTime=60,
+    maxGens=500, stallGenMax=100, selPress=1.2, elitism=5, relTol=0.1,
+    verbose=TRUE, priorBitString=NULL, maxSizeHashTable=5000){
+
+    warning("gaBinaryT2 is a deprecated function. Use gaBinaryTN instead.")
+    return(gaBinaryTN(CNOlist=CNOlist, model=model, bStrings=list(bStringT1),
+     sizeFac=sizeFac, NAFac=NAFac, popSize=popSize,  pMutation=pMutation,  maxTime=maxTime,
+     maxGens=maxGens,  stallGenMax=stallGenMax,  selPress=selPress,  elitism=elitism,
+     relTol=relTol,  verbose=verbose, priorBitString=priorBitString,
+maxSizeHashTable=maxSizeHashTable))
+}
 
