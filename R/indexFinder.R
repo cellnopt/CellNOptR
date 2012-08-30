56e0f4699bc864669601eb8ca6c5198b661616d5
Thomas Cokelaer <cokelaer@gmail.com>
2012-08-30 12:45:05 +0000

    uses new CNOlist class.
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@2266 be9b9435-a577-4725-b17d-0f23881b854c

--- R/indexFinder.R
+++ R/indexFinder.R
@@ -15,21 +15,10 @@
 # $Id$
 indexFinder<-function(CNOlist, model, verbose=FALSE){
 
-#check that CNOlist is a CNOlist
-    if(!is.list(CNOlist)){
+    if ((class(CNOlist)=="CNOlist")==FALSE){
+        CNOlist = CellNOptR::CNOlist(CNOlist)
+    } else {
         stop("This function expects as input a CNOlist as output by makeCNOlist or normaliseCNOlist")
-        }
-    if(all(names(CNOlist) != c(
-        "namesCues",
-        "namesStimuli",
-        "namesInhibitors",
-        "namesSignals",
-        "timeSignals",
-        "valueCues",
-        "valueInhibitors",
-        "valueStimuli",
-        "valueSignals"))){
-        stop("This function expects as input a CNOlist as output by makeCNOlist")
         }
 
     #check that Model is a model list
@@ -53,13 +42,13 @@ indexFinder<-function(CNOlist, model, verbose=FALSE){
         }
 
     #Find the indexes of the signals
-    signals<-match(CNOlist$namesSignals,model$namesSpecies)
+    signals<-match(colnames(CNOlist@signals[[1]]),model$namesSpecies)
 
     #Find the indexes of the stimulated species
-    stimulated<-match(CNOlist$namesStimuli,model$namesSpecies)
+    stimulated<-match(colnames(CNOlist@stimuli),model$namesSpecies)
 
     #Find the indexes of the inhibited species
-    inhibited<-match(CNOlist$namesInhibitors,model$namesSpecies)
+    inhibited<-match(colnames(CNOlist@inhibitors),model$namesSpecies)
 
     #Print summaries
     if(verbose){
