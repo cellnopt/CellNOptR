39d3bb1a310a829aa23d7e461b1afbcb24b95db5
Aidan Macnamara <aidan.macnamara@ebi.ac.uk>
2012-09-18 15:16:39 +0000

    cnordt updates
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CNOR_dt@2440 be9b9435-a577-4725-b17d-0f23881b854c

---
+++ CNORdt/R/cutAndPlotResultsDT.R
@@ -0,0 +1,124 @@
+ #
+#  This file is part of the CNO software
+#
+#  Copyright (c) 2011-2012 - EBI
+#
+#  File author(s): CNO developers (cno-dev@ebi.ac.uk)
+#
+#  Distributed under the GPLv2 License.
+#  See accompanying file LICENSE.txt or copy at
+#      http://www.gnu.org/licenses/gpl-2.0.html
+#
+#  CNO website: http://www.ebi.ac.uk/saezrodriguez/software.html
+#
+##############################################################################
+# $Id: $
+
+cutAndPlotResultsDT <- function(model, bString, simList=NULL, CNOlist, indexList=NULL,
+ plotPDF=FALSE, tag=NULL, plotParams=list(maxrow=10),boolUpdates=30, lowerB=0.8, upperB=10)
+{
+	
+    if ((class(CNOlist)=="CNOlist")==FALSE){
+        CNOlist = CellNOptR::CNOlist(CNOlist)
+    } 
+    if (is.null(simList)==TRUE){
+        simList = prep4sim(model)
+    }
+    if (is.null(indexList)==TRUE){
+        indexList = indexFinder(CNOlist, model)
+    }
+
+    if ("maxrow" %in% names(plotParams) == FALSE){
+        plotParams$maxrow = 10
+    }
+
+    # keep simList and indxList for back compatibility ?
+    modelCut <- cutModel(model, bString)
+    simListCut <- cutSimList(simList, bString)
+
+    # t0
+    Sim0 <- simulatorT0(CNOlist=CNOlist, model=modelCut, simList=simListCut, indexList=indexList)
+
+	# make simulatorDT compatible with class
+    Sim <- simulatorDT(CNOlist=CNOlistPB, model=modelCut, simList=simListCut, indices=indexList, boolUpdates=boolUpdates, prevSim = Sim0) 
+    
+    simResults = convert2array(Sim, dim(CNOlist@signals[[1]])[1], length(model$namesSpecies), boolUpdates)
+
+    optimResults <- getFitDT(simResults = simResults, CNOlist = CNOlistPB, model = modelCut, indexList = indexList, sizeFac = sizeFac, 
+        NAFac = NAFac, nInTot = length(which(model$interMat == -1)), boolUpdates, divTime = NULL, lowerB = lowerB, upperB = upperB)
+	    simResults <- simResults[, indexList$signals, ]
+
+    dim1 = dim(CNOlist@signals[[1]])[1]
+    dim2 = dim(CNOlist@signals[[1]])[2]
+
+    CNOlistSet = list()
+    simResultsSet = list()
+
+    if(dim1 > plotParams$maxrow) { #|| dim2 > 10) {
+
+        par1 = ceiling(dim1/plotParams$maxrow)
+        div1 = ceiling(dim1/par1)
+        #par2 = ceiling(dim2/plotParams$maxrow)
+        #div2 = ceiling(dim2/par2)
+
+        count1 = 1
+        for(a in 1:par1) {
+            CNOdiv = CNOlist
+            simDiv = simResults
+            finalN = div1 * a
+            if(finalN > dim1) {finalN = dim1}
+            CNOdiv@cues = CNOdiv@cues[count1:finalN,]
+            CNOdiv@stimuli = CNOdiv@stimuli[count1:finalN,]
+            CNOdiv@inhibitors = CNOdiv@inhibitors[count1:finalN,]
+            for(b in 1:length(CNOdiv@signals)) {
+                CNOdiv@signals[[b]] = CNOdiv@signals[[b]][count1:finalN,]
+            }
+            for(d in 1:length(simDiv)) {
+                simDiv[[d]] = simDiv[[d]][count1:finalN,]
+            }
+            count1 = count1 + div1
+            CNOlistSet = c(CNOlistSet, list(CNOdiv))
+            simResultsSet = c(simResultsSet, list(simDiv))
+        }
+    } else {
+
+        CNOlistSet = list(CNOlist)
+        simResultsSet = list(simResults)
+    }
+
+    outputFilenames = list()
+    for(f in 1:length(CNOlistSet)) {
+
+        plotOptimResultsPan(
+            simResults=simResultsSet[[f]],
+            CNOlist=CNOlistSet[[f]],
+        	yInterpol=optimResults$yInter,
+			xCoords=optimResults$xCoords,
+			formalism="dt",
+			tPt=CNOlist@timepoints,
+            plotParams=plotParams
+            )
+
+        if(plotPDF == TRUE) {
+            if(is.null(tag)) {
+                filename <- paste("SimResultsT1_", f, ".pdf", sep="")
+            } else {
+                filename <- paste(tag,"SimResultsT1",f,".pdf",sep="_")
+            }
+            plotOptimResultsPan(
+                 simResults=simResultsSet[[f]],
+            CNOlist=CNOlistSet[[f]],
+        	yInterpol=optimResults$yInter,
+			xCoords=optimResults$xCoords,
+			formalism="dt",
+			tPt=CNOlist@timepoints,
+            plotParams=plotParams,
+                            pdf=TRUE,
+                pdfFileName=filename
+
+            )
+            outputFilenames[[f]] = filename
+        }
+    }
+    return(outputFilenames)
+}
--- CNORdt/inst/doc/CNORdt-vignette-example.R
+++ CNORdt/inst/doc/CNORdt-vignette-example.R
@@ -1,64 +1,78 @@
-# check
 library(CellNOptR)
 
 # for later
 library(CNORdt)
 
 # check data names / documentation
-data(CNOlistPB; package="CNORdt")
-data(ToyModelPB; package="CNORdt")
+data(CNOlistPB, package="CNORdt")
+data(ToyModelPB, package="CNORdt")
 
 load("data/CNOlistPB.RData")
 load("data/modelPB.RData")
 
 # pre-process model
-
-model = preprocessing(CNOlistPB; modelPB)
+model = preprocessing(CNOlistPB, modelPB)
 
 # optimise
-initBstring <- rep(1; length(model$reacID))
+initBstring <- rep(1, length(model$reacID))
 
 source("R/gaBinaryDT.R")
 source("R/getFitDT.R")
 source("R/simulatorDT.R")
+source("R/computeScoreDT.R")
 dyn.load("src/simulatorDT.so")
 
-opt1 <- gaBinaryDT(CNOlist=CNOlist; model=ModelCutCompressExpand;
-simList=fields4Sim; indexList=indexNONCcutComp; initBstring=initBstring;
-verbose=TRUE; boolUpdates=c(10;20); divTime=10; MaxTime=100; lowerB=0.8; upperB=10)
 
-cutAndPlotResultsTimeScale(model=ModelCutCompressExpand; bString=opt1$bString; simList=fields4Sim;
-CNOlist=CNOlist; indexList=indexNONCcutComp; boolUpdates=c(10;20); divTime=10; lowerB=0.8; upperB=10)
+opt1 <- gaBinaryDT(CNOlist=CNOlistPB, model=model, initBstring=initBstring,
+verbose=TRUE, boolUpdates=30, divTime=10, maxTime=30, lowerB=0.8, upperB=10)
 
-dataStartPoint = cutModel(model=ModelCutCompressExpand;
-SimList=fields4Sim; bitString=opt1$bString)
-simT1 = simulatorTimeScale(CNOlist=CNOlist;
-model=dataStartPoint[[1]]; simList=dataStartPoint[[2]];
-indexList=indexNONCcutComp; boolUpdates=10)
+source("R/cutAndPlotResultsDT.R")
+cutAndPlotResultsDT(
+	model=model,
+	CNOlist=CNOlistPB,
+	bString=opt1$bString,
+	plotPDF=FALSE)
 
-opt2 <- gaBinaryTimeScaleT2(CNOlist=CNOlist;
-model=ModelCutCompressExpand; simList=fields4Sim;
-indexList=indexNONCcutComp; bStringT1=opt1$bString;
-simResT1=simT1; verbose=TRUE; boolUpdates=c(10;20);
-divTime=10; lowerB=0.8; upperB=10)
 
-cutAndPlotResultsTimeScaleT2(model=ModelCutCompressExpand;
-bStringT1=opt1$bString; bStringT2=opt2$bString; simList=fields4Sim;
-CNOlist=CNOlist; indexList=indexNONCcutComp; boolUpdates=c(10;20);
-divTime=10; lowerB=0.8; upperB=10)
+
+
+
+
+
+
+
+
+
+
+dataStartPoint = cutModel(model=ModelCutCompressExpand,
+SimList=fields4Sim, bitString=opt1$bString)
+simT1 = simulatorTimeScale(CNOlist=CNOlist,
+model=dataStartPoint[[1]], simList=dataStartPoint[[2]],
+indexList=indexNONCcutComp, boolUpdates=10)
+
+opt2 <- gaBinaryTimeScaleT2(CNOlist=CNOlist,
+model=ModelCutCompressExpand, simList=fields4Sim,
+indexList=indexNONCcutComp, bStringT1=opt1$bString,
+simResT1=simT1, verbose=TRUE, boolUpdates=c(10,20),
+divTime=10, lowerB=0.8, upperB=10)
+
+cutAndPlotResultsTimeScaleT2(model=ModelCutCompressExpand,
+bStringT1=opt1$bString, bStringT2=opt2$bString, simList=fields4Sim,
+CNOlist=CNOlist, indexList=indexNONCcutComp, boolUpdates=c(10,20),
+divTime=10, lowerB=0.8, upperB=10)
 
 writeScaffold(
-	modelComprExpanded=ModelCutCompressExpand;
-	optimResT1=opt1;
-	optimResT2=opt2;
-	modelOriginal=Model;
+	modelComprExpanded=ModelCutCompressExpand,
+	optimResT1=opt1,
+	optimResT2=opt2,
+	modelOriginal=Model,
 	CNOlist=CNOlist
 )
 	
 writeNetwork(
-	modelOriginal=Model;
-	modelComprExpanded=ModelCutCompressExpand;
-	optimResT1=opt1;
-	optimResT2=opt2;
+	modelOriginal=Model,
+	modelComprExpanded=ModelCutCompressExpand,
+	optimResT1=opt1,
+	optimResT2=opt2,
 	CNOlist=CNOlist
 )
