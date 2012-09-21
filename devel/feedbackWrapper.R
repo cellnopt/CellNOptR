17b56c35a52d904b07bca213107e40ff18bdcdde
Aidan Macnamara <aidan.macnamara@ebi.ac.uk>
2012-09-21 09:46:43 +0000

    cnordt cleanup
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CNOR_dt@2464 be9b9435-a577-4725-b17d-0f23881b854c

--- devel/feedbackWrapper.R
+++ devel/feedbackWrapper.R
@@ -2,23 +2,31 @@ feedbackWrapper <- function(model) {
 
 	whatLoops = feedbackFinder1(model)
 	negReacs = c()
-	for(b in 1:length(whatLoops)) {
-		loop1 = whatLoops[[b]]
-		loop1 = loop1[loop1 > 0]
-		loop1 = c(loop1, loop1[1])
-		for(a in 1:length(loop1)-1) {
-			lhs = loop1[a]
-			rhs = loop1[a+1]
-			LHSreac = which(model$interMat[lhs,] == -1)
-			RHSreac = which(model$interMat[rhs,] == 1)
-			reac = intersect(LHSreac, RHSreac)
-			if(any(model$notMat[,reac] == 1)) {
-				negReacs = c(negReacs, reac)	
+	
+	if(length(whatLoops) != 0) {
+		
+		for(b in 1:length(whatLoops)) {
+			loop1 = whatLoops[[b]]
+			loop1 = loop1[loop1 > 0]
+			loop1 = c(loop1, loop1[1])
+			for(a in 1:length(loop1)-1) {
+				lhs = loop1[a]
+				rhs = loop1[a+1]
+				LHSreac = which(model$interMat[lhs,] == -1)
+				RHSreac = which(model$interMat[rhs,] == 1)
+				reac = intersect(LHSreac, RHSreac)
+				if(any(model$notMat[,reac] == 1)) {
+					negReacs = c(negReacs, reac)	
+				}
 			}
 		}
+		negReacs = unique(negReacs)
+		negEdges = rep(0,length(model$reacID))
+		negEdges[negReacs] = 1
+		return(negEdges)
+	} else {
+		
+		print("No loops found!")
+		return(NULL)	
 	}
-	negReacs = unique(negReacs)
-	negEdges = rep(0,length(model$reacID))
-	negEdges[negReacs] = 1
-	return(negEdges)
 }
