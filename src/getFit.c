515c1aefa4849da347f5131c47cd172ce127e5ef
Thomas Cokelaer <cokelaer@gmail.com>
2012-12-06 10:32:02 +0000

    fix typo.
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CellNOptR@2899 be9b9435-a577-4725-b17d-0f23881b854c

--- src/getFit.c
+++ src/getFit.c
@@ -132,7 +132,7 @@ SEXP getFit (
 	    for (j = 0; j < nSignals; j++) {
             if (time0 == 1){
 	            r =  simResT0[i][j] - cnolist0[i][j];
-                if (!ISNA(r) && isNAN(r)){
+                if (!ISNA(r) && ISNAN(r)){
                     deviationPen += r*r;
                     // TC, nov.2012 why are we not counting nDataP for time zero as well ? 
                 }
