13b2a26f62e55dac91e016eeb5a688581da65f7b
Thomas Cokelaer <cokelaer@gmail.com>
2013-01-09 16:04:09 +0000

    update headerss
    git-svn-id: https://svn.ebi.ac.uk/sysbiomed/trunk/CNOR_ode@3157 be9b9435-a577-4725-b17d-0f23881b854c

--- CNORode/R/defaultParametersSSm.R
+++ CNORode/R/defaultParametersSSm.R
@@ -1,15 +1,15 @@
 #
 #  This file is part of the CNO software
 #
-#  Copyright (c) 2011-2012 - EBI
+#  Copyright (c) 2011-2013 - EBI
 #
 #  File author(s): CNO developers (cno-dev@ebi.ac.uk)
 #
-#  Distributed under the GPLv2 License.
+#  Distributed under the GPLv3 License.
 #  See accompanying file LICENSE.txt or copy at
-#      http://www.gnu.org/licenses/gpl-2.0.html
+#      http://www.gnu.org/licenses/gpl-3.0.html
 #
-#  CNO website: http://www.ebi.ac.uk/saezrodriguez/softare/cno
+#  CNO website: http://www.cellnopt.org
 #
 ##############################################################################
 # $Id: defaultParametersGA.R 2337 2012-09-06 09:00:07Z davidh $
